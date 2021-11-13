d43abd66701f358d19798ccbaf9755da04fecba2
eticialima <leticialimacgi@gmail.com>
2021-11-13 18:15:29 -0300

    post Atualizado

--- portal/home/urls.py
+++ portal/home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
-from home.views import (IndexHomelView, HomeView)
+from home.views import (IndexHomelView, HomeView, DetailView)
 
 urlpatterns = [
 	path('', HomeView.as_view(), name='home'),
 	path('index-home/', IndexHomelView.as_view(), name='index-home'), 
+	path('post-detail/<int:pk>/', DetailView.as_view(), name='post-detail'),
 ] 
--- portal/home/views.py
+++ portal/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render 
-from django.views.generic.base import TemplateView 
+from django.views.generic.base import TemplateView
+from django.views.generic import DetailView
 from base.base_admin_permissions import BaseAdminUsersall
 from post.models import Post
 from post.forms import PostForm
@@ -16,3 +17,8 @@ class HomeView(TemplateView):
 		context = super().get_context_data(**kwargs)
 		context['post_list'] = self.model.objects.all()
 		return context 
+
+class DetailView(DetailView):
+	model = Post
+	form_class = PostForm
+	template_name = 'home/post_detail.html'     
